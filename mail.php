43ed53b8166e80a77af9d6a92e7bdb084b487025
craftyoyo <ysys9696@live.fr>
2019-10-16 23:20:28 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 
 if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response']))
 {
-      $secret = '6LcA9b0UAAAAAJ6nLzeHmUQ5nhhzdOtDxlmcooYj';
+      $secret = 'nope';
       $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
       $responseData = json_decode($verifyResponse);
       if($responseData->success)
